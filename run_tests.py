843df98aa1fc1c03b3eaae93b7e6d610ec80bedc
Benjamin Smith <bsmith@cabbage.org.uk>
2023-01-27 09:28:14 +0000

    Example data and example test

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+from tests.exchange_rate_test import TestExchangeRate
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/exchange_rate_test.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import date
+
+from exchange_rate import ExchangeRate
+
+class TestExchangeRate(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_at_example_1(self):
+        # GBP is the base current and USD is the counter currency
+        # returns how many GBP one USD is worth
+        rate = ExchangeRate.at(date(2023,1,26), 'GBP', 'USD')
+        # from example.xml:
+        #   20230126 1 EUR = 0.8794 GBP
+        #   20230126 1 EUR = 1.0895 USD
+        # (1 USD) / (1.0895 USD/EUR) * (0.8794 GBP/EUR)
+        self.assertAlmostEqual(0.80716, rate, decimal=4)
